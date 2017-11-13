26b70245698352c7bb577439e1e7efad6e5902dc
Klaus Wenger <kjwenger@yahoo.com>
2017-11-13 17:23:50 +0100

    fixed for build on raspberry pi

--- app/src/Mosquitto.cpp
+++ app/src/Mosquitto.cpp
@@ -3,20 +3,21 @@
 Mosquitto::Mosquitto(const char * id, const char * host, int port)
         : mosqpp::mosquittopp(id) {
     const int keep_alive = 60;
-    const bool clean_session = true;
 
     mosqpp::mosquittopp::connect(host, port, keep_alive);
 }
 Mosquitto::~Mosquitto() {
-
+    mosqpp::mosquittopp::disconnect();
 }
 
 void Mosquitto::on_connect(int rc) {
-
+    (void) rc;
 }
-void Mosquitto::on_message(const struct mosquitto_message *message) {
-
+void Mosquitto::on_message(const struct mosquitto_message * message) {
+    (void) message;
 }
 void Mosquitto::on_subscribe(int mid, int qos_count, const int * granted_qos) {
-
+    (void) mid;
+    (void) qos_count;
+    (void) granted_qos;
 }
